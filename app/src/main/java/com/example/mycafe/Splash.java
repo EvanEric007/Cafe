c2113e661dd7ed61df3517c5a8ff628d955b504f
EvanEric007 <bitm-f19-007@superior.edu.pk>
2022-03-01 10:46:06 +0500

    Dashboard

---
+++ app/src/main/java/com/example/mycafe/Splash.java
@@ -0,0 +1,38 @@
+package com.example.mycafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.window.SplashScreen;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(3000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(),MainActivity.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+                } catch (Exception e) {
+                    e.getStackTrace();
+                }
+            }
+        };
+        // start thread
+        background.start();
+
+    }
+}
